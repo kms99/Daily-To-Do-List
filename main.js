fdcff32f8bc4a29d6fbb3db61f4099d592933805
Kim MyungSub <audtjqxx@naver.com>
2023-09-09 23:34:57 +0900

    추가하기 버튼 생성, 할일 추가 이벤트 구현

--- main.js
+++ main.js
@@ -11,8 +11,7 @@
  */
 
 /* 모드 체인지 이벤트 */
-let startTimer={};
-
+// let startTimer={};
 let mode='dark';
 document.getElementById('changeModeBtn').addEventListener('click',function(){
     if(mode==='dark'){
@@ -25,6 +24,7 @@ document.getElementById('changeModeBtn').addEventListener('click',function(){
             item.classList.replace('d-mode-list-item','w-mode-list-item');
         })
         mode = 'white';
+
     }else if (mode==='white'){
         document.querySelector('body').classList.replace('w-mode-body','d-mode-body');
         document.getElementById('topArea').classList.replace('w-mode-top','d-mode-top');
@@ -49,55 +49,111 @@ document.querySelectorAll('#navigation button').forEach((button)=>{
     })
 })
 
-
-
-
-
-
-
-
-
-
-let listItem = document.querySelectorAll('.list-item');
-
-listItem.forEach((item)=>{
-    let stopWatchControl = item.querySelector('.stopWatch');
-    let itemTitle = item.querySelector('#item-title');
-    stopWatchControl.querySelector('#playBtn').addEventListener('click',function(){
-        console.log(`${itemTitle.innerHTML}시작`);
-        stopWatch(itemTitle.innerHTML);
-    })
+/* list input area open */
+document.getElementById('add-area-open').addEventListener('click',function(){
+    const inputArea= document.getElementById('add-input');
+    if (!inputArea.style.maxWidth || inputArea.style.maxWidth==='0px'){
+        inputArea.style.maxWidth = '100vw';
+        document.getElementById('add-area-open').innerHTML=`<i class="fa-solid fa-x"></i></i>
+        닫기`;
+    }else{
+        inputArea.style.maxWidth='0px';
+        document.getElementById('add-area-open').innerHTML=`<i class="fa-solid fa-plus"></i>
+        추가하기`;
+    }  
 })
 
-listItem.forEach((item)=>{
-    let stopWatchControl = item.querySelector('.stopWatch');
-    let itemTitle = item.querySelector('#item-title');
-    stopWatchControl.querySelector('#pauseBtn').addEventListener('click',function(){
-        console.log(`${itemTitle.innerHTML}끝`);
-        clearInterval(startTimer[itemTitle.innerHTML])
+
+/* add items */
+let userList = [];
+function addItem (){
+    const inputText = document.getElementById('user-input').value;
+    userList.push(inputText);
+    let listHtml =''; 
+    userList.forEach((item,index)=>{
+        listHtml += 
+            `<div class="d-mode-list-item row list-item">
+                <div class="col-md-1">${index+1}</div>
+                <div class="col-md-6" id="item-title">${item}</div>
+                <div class="col-md-4 stopWatch" id="stopWatch">
+                <i class="fa-regular fa-clock stopWatchBtn" id="stopWatchBtn"></i>
+                <div>00:00:00</div>
+                <i class="fa-solid fa-play" id="playBtn"></i>
+                <i class="fa-solid fa-pause" id="pauseBtn"></i>
+                </div>
+                <i class="fa-regular fa-square-check col-md-1"></i>
+            </div>
+            `
     })
+    document.getElementById('list-area').innerHTML=listHtml;
+}
+
+document.getElementById('user-input').addEventListener('keypress',function(e){
+    if(e.key=='Enter'){
+        addItem();
+    }
 })
+
+document.getElementById('add-btn').addEventListener('click',function(){
+    addItem();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let listItem = document.querySelectorAll('.list-item');
+
+// listItem.forEach((item)=>{
+//     let stopWatchControl = item.querySelector('.stopWatch');
+//     let itemTitle = item.querySelector('#item-title');
+//     stopWatchControl.querySelector('#playBtn').addEventListener('click',function(){
+//         console.log(`${itemTitle.innerHTML}시작`);
+//         let timerInfo= stopWatch(itemTitle.innerHTML);
+//         console(timerInfo);
+//     })
+// })
+
+// listItem.forEach((item)=>{
+//     let stopWatchControl = item.querySelector('.stopWatch');
+//     let itemTitle = item.querySelector('#item-title');
+//     stopWatchControl.querySelector('#pauseBtn').addEventListener('click',function(){
+//         console.log(`${itemTitle.innerHTML}끝`);
+//         clearInterval(startTimer[itemTitle.innerHTML])
+//     })
+// })
 
 /* 스톱워치 기능 */
-function stopWatch (title) {
-    console.log(startTimer);
+// function stopWatch (title) {
+//     // console.log(startTimer);
 
-    let hour = 0;
-    let second = 0;
-    let minute = 0;
-    let time = 0;
-    // let time = startTimer[title][1]||0;
+//     let hour = 0;
+//     let second = 0;
+//     let minute = 0;
+//     let time = 0;
+//     // let time = startTimer[title][1]||0;
 
-    let timer= setInterval(function(){
-        time++;
-        second = time%60;
-        minute = Math.floor(time/60);
-        hour = Math.floor(minute/60);
-        minute = minute%60; 
-        startTimer[title] = 
-        console.log(title, hour, minute, second);
-    },1000);
+//     let timer= setInterval(function(){
+//         time++;
+//         second = time%60;
+//         minute = Math.floor(time/60);
+//         hour = Math.floor(minute/60);
+//         minute = minute%60; 
 
-    startTimer[title] = [timer, time];
-    console.log(startTimer);
-}
+//         startTimer[title] = [timer, time];
+
+//         console.log(title, hour, minute, second);
+//     },1000);
+
+
+//     startTimer[title] = [timer, time];
+//     console.log(startTimer);
+// }
